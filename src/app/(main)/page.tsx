e2c76ef406bc2963fa0763548a13f4e1c7f58b42
infrasync <dev@ron.my.id>
2024-08-08 08:42:13 +0700

    :bug: fix(app): suspense on main page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from 'react';
+
 import HomePageFeatures from '@/components/features/HomePageFeatures';
 import PageLayout from '@/components/layouts/PageLayout';
 
 export default async function HomePage() {
   return (
-    <PageLayout>
-      <HomePageFeatures />
-    </PageLayout>
+    <Suspense>
+      <PageLayout>
+        <HomePageFeatures />
+      </PageLayout>
+    </Suspense>
   );
 }
